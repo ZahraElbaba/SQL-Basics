bd05320d1a99e6b205af694f191ceddcbd06dbd3
Zahra El Baba <zahrababa73@hotmail.com>
2024-12-11 13:40:07 +0200

    answers.sh

---
+++ answers.sh
@@ -0,0 +1,21 @@
+"SELECT name from students;"
+"SELECT * FROM students WHERE age > 30;"
+"SELECT name FROM students WHERE Gender = 'F' and age = 30;"
+"SELECT points FROM students WHERE name = 'Alex';"
+"INSERT INTO students (name, age , Gender) VALUES ('zahra', 23, 'F');"
+"UPDATE students SET points = points + 20 WHERE name = 'Basma';"
+"UPDATE students SET points = points - 5 WHERE name = 'Alex';"
+
+"INSERT INTO graduates (Name,Age,Gender,points) SELECT Name, Age, Gender, Points FROM students WHERE Name = 'layal';"
+"UPDATE graduates SET Graduation = 'August-09-2018' WHERE name = 'Layal';"
+"DELETE FROM students WHERE name='Layal'"
+
+"SELECT employees.Name  , employees.Company  ,companies.Date from employees,companies where employees.Company=companies.Name;"
+"SELECT employees.Name from employees,companies where employees.Company=companies.Name and companies.Date<2000;"
+"SELECTelect Company from employees WHERE role='Graphic Designer'"
+
+"SELECT Name from students where Points=(select max(points) FROM students);"
+"SELECT avg(points)FROM students;"
+"SELECT count(*)FROM students where points=500;"
+"SELECT name FROM students WHERE Name like '%s%';" 
+"SELECT * FROM students ORDER by Points DESC;"
